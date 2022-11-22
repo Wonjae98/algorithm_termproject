f9912937634bb54b1a583f6f4b6232fb64fbbdfe
wonjae98 <dnjswo980213@gmail.com>
2022-11-23 01:15:02 +0900

    -Layout- 1. 하단 바 추가 2. 각 레이아웃들 아래에 fragment 있도록 설정

---
+++ app/src/main/java/com/example/algorithmproject/ResultActivity.java
@@ -0,0 +1,17 @@
+package com.example.algorithmproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.result);
+
+    }
+}
